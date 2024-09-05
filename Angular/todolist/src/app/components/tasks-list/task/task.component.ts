1ca8f1fa209bb793a0bb3a901d2583afbaf08c53
Ramzi <ramzibenmansour.pro@gmail.com>
2024-09-05 14:50:58 +0200

    Styling

--- Angular/todolist/src/app/components/tasks-list/task/task.component.ts
+++ Angular/todolist/src/app/components/tasks-list/task/task.component.ts
@@ -1,21 +1,52 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {TaskInterface} from "../../../interfaces/task-interface";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {TaskService} from "../../../services/Task-service";
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
-export class TaskComponent {
+export class TaskComponent implements OnInit{
   @Input() task!: TaskInterface;
   @Output() statusChange = new EventEmitter<string>();
+  updateForm!: FormGroup;
+  isUpdating: boolean = false;
+
+  constructor(private fb: FormBuilder, private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.updateForm = this.fb.group({
+      name: [this.task.name, Validators.required],
+      done: [this.task.done],
+      comment: [this.task.comment],
+      cost: [this.task.cost],
+    });
+  }
 
   toggleTaskStatus() {
-    console.log('Toggling status for task:', this.task.id); // Debug log
-
     this.statusChange.emit(this.task.id);
   }
+
+  toggleUpdateForm() {
+    this.isUpdating = !this.isUpdating;
+  }
+
+  onSubmitUpdate() {
+    if (this.updateForm.valid) {
+      const updatedTaskData: Partial<TaskInterface> = this.updateForm.value;
+      this.taskService.patchTask(this.task.id, updatedTaskData).subscribe({
+        next: (updatedTask) => {
+          Object.assign(this.task, updatedTask);
+          this.isUpdating = false;
+        },
+        error: (error) => console.error('Erreur lors de la mise à jour de la tâche :', error)
+      });
+    }
+  }
+
 }
