73383eb48a75f8d62008a384ec32c633caa446ff
Ramzi <ramzibenmansour.pro@gmail.com>
2024-09-06 14:49:10 +0200

    feat: add NotFoundComponent for handling 404 errors with redirection to product list

--- Angular/angularcrud/src/app/app.routes.ts
+++ Angular/angularcrud/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import {ProductAddComponent} from "./product-add/product-add.component";
 import {ProductEditComponent} from "./product-edit/product-edit.component";
 import {ProductGetComponent} from "./product-get/product-get.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'get', pathMatch: 'full' },
   { path: 'add', component: ProductAddComponent },
-  { path: 'edit:id', component: ProductEditComponent },
+  { path: 'edit/:id', component: ProductEditComponent },
   { path: 'get', component: ProductGetComponent },
-  /*get one*/
-
+  { path: '**', component: NotFoundComponent }
 ];
