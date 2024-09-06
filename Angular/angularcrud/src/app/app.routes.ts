dbebb3155dbfc082bb40223e1a847561ffaceb24
Ramzi <ramzibenmansour.pro@gmail.com>
2024-09-06 10:22:29 +0200

    Set up necessary components, routes and interfaces

--- Angular/angularcrud/src/app/app.routes.ts
+++ Angular/angularcrud/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {ProductAddComponent} from "./product-add/product-add.component";
+import {ProductEditComponent} from "./product-edit/product-edit.component";
+import {ProductGetComponent} from "./product-get/product-get.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'add', component: ProductAddComponent },
+  { path: 'edit:id', component: ProductEditComponent },
+  { path: 'get', component: ProductGetComponent },
+
+];
---
+++ Angular/angularcrud/src/app/product-get/product-get.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-get',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-get.component.html',
+  styleUrl: './product-get.component.css'
+})
+export class ProductGetComponent {
+
+}
